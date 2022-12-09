064edd8f8900761eb58851ce9828790e43106735
Joel Alves Pereira <atejap05@yahoo.com.br>
2022-12-09 15:49:57 -0400

    implementa tela de Manifestacoes (Historico Pessoal)

--- src/pages/permutas/Manifestacoes.jsx
+++ src/pages/permutas/Manifestacoes.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { Space, Table, Tag } from "antd";
+const { Column, ColumnGroup } = Table;
 
-const Manifestacoes = () => {
-  return <div>Manifestacoes</div>;
-};
+const data = [
+  {
+    key: "1",
+    dataInclusao: "01/01/01",
+    unidadeOrigem: "DRF-Cuiabá",
+    ufOrigem: "MT",
+    unidadeDestino: "DRF-Sao Jose dos Campos",
+    ufDestino: "SP",
+  },
+  {
+    key: "2",
+    dataInclusao: "01/01/01",
+    unidadeOrigem: "DRF-Cuiabá",
+    ufOrigem: "MT",
+    unidadeDestino: "DRF-Nova York",
+    ufDestino: "NY",
+  },
+];
 
+const Manifestacoes = () => (
+  <Table dataSource={data}>
+    <ColumnGroup title="DADOS DA ORIGEM">
+      <Column
+        title="Data da Inclusão"
+        dataIndex="dataInclusao"
+        key="dataInclusao"
+      />
+      <Column title="Unidade" dataIndex="unidadeOrigem" key="unidadeOrigem" />
+      <Column title="UF" dataIndex="ufOrigem" key="ufOrigem" />
+    </ColumnGroup>
+    <ColumnGroup title="DADOS DO DESTINO">
+      <Column title="Unidade" dataIndex="unidadeDestino" key="unidadeDestino" />
+      <Column title="UF" dataIndex="ufDestino" key="ufDestino" />
+      <Column
+        title="Action"
+        key="action"
+        render={(_, record) => (
+          <Space size="middle">
+            <a>Detalhes</a>
+          </Space>
+        )}
+      />
+    </ColumnGroup>
+  </Table>
+);
 export default Manifestacoes;
