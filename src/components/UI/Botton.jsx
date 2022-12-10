8c2f457e04aab3bfafb9820a9f0f9f5c0126f490
Joel Alves Pereira <atejap05@yahoo.com.br>
2022-12-10 14:16:42 -0400

    implementa Not Found Page

--- src/components/UI/Botton.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Botton = () => {
-  return <div>Botton</div>;
-};
-
-export default Botton;
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import classes from "../css/styles.module.css";
+import AntButton from "../components/UI/AntButton";
+import { useNavigate } from "react-router-dom";
 
-const Error = () => {
-  return <div>Error</div>;
+const ErrorPage = () => {
+  const navigate = useNavigate();
+  return (
+    <section className={classes["errorpage"]}>
+      <AntButton type={"primary"} onClick={() => navigate("/")}>
+        &larr; Go Home
+      </AntButton>
+    </section>
+  );
 };
 
-export default Error;
+export default ErrorPage;
