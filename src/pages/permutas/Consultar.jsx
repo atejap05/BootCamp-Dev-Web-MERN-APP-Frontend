15f6b9906bc69cecdaea937cfc2d2008a7c92930
Joel Alves Pereira <atejap05@yahoo.com.br>
2022-12-10 11:22:15 -0400

    pagina consulta intencoes

--- src/pages/permutas/Consultar.jsx
+++ src/pages/permutas/Consultar.jsx
@@ -2,9 +2,32 @@ import React, { useState, useEffect } from "react";
 import classes from "../../css/styles.module.css";
 import { Button } from "antd";
 import AntdSelect from "../../components/UI/AntSelect";
-
+import { Space, Table } from "antd";
 import api from "../../api/api";
 const UNIDADES_CACHE = {};
+const { Column, ColumnGroup } = Table;
+
+// Table results
+const data = [
+  {
+    key: "1",
+    cpf: "***.***.683-72",
+    dataInclusao: "01/01/01",
+    unidadeOrigem: "DRF-Cuiabá",
+    ufOrigem: "MT",
+    unidadeDestino: "DRF-Sao Jose dos Campos",
+    ufDestino: "SP",
+  },
+  {
+    key: "2",
+    cpf: "***.***.683-72",
+    dataInclusao: "01/01/01",
+    unidadeOrigem: "DRF-Cuiabá",
+    ufOrigem: "MT",
+    unidadeDestino: "DRF-Nova York",
+    ufDestino: "NY",
+  },
+];
 
 const Consultar = () => {
   const [unidades, setUnidades] = useState([]);
@@ -25,17 +48,17 @@ const Consultar = () => {
   const onIncluirHandler = () => {};
 
   return (
-    <section className={classes["incluir"]}>
-      <div className={classes["incluir__title"]}>
+    <section className={classes["consultar"]}>
+      <div className={classes["consultar__title"]}>
         <h1>Consulta Destino Disponíveis</h1>
         <p>
           Nome: <span>Joel Alves Pereira</span>
         </p>
       </div>
 
-      <div className={classes["incluir__form"]}>
+      <div className={classes["consultar__form"]}>
         <h3>Dados de Destino</h3>
-        <div>
+        <div className={classes["consultar__selects"]}>
           <AntdSelect
             onSelectChange={value => {
               if (UNIDADES_CACHE.hasOwnProperty(value.value)) {
@@ -69,6 +92,43 @@ const Consultar = () => {
             Consultar
           </Button>
         </div>
+      </div>
+      <div className={classes["consultar__table"]}>
+        <p>Intenões de Permuta para:</p>
+        <br />
+        <Table dataSource={data}>
+          <ColumnGroup title="DADOS DA ORIGEM">
+            <Column
+              title="Data da Inclusão"
+              dataIndex="dataInclusao"
+              key="dataInclusao"
+            />
+            <Column title="CPF" dataIndex="cpf" key="cpf" />
+            <Column
+              title="Unidade"
+              dataIndex="unidadeOrigem"
+              key="unidadeOrigem"
+            />
+            <Column title="UF" dataIndex="ufOrigem" key="ufOrigem" />
+          </ColumnGroup>
+          <ColumnGroup title="DADOS DO DESTINO">
+            <Column
+              title="Unidade"
+              dataIndex="unidadeDestino"
+              key="unidadeDestino"
+            />
+            <Column title="UF" dataIndex="ufDestino" key="ufDestino" />
+            <Column
+              title="Action"
+              key="action"
+              render={(_, record) => (
+                <Space size="middle">
+                  <a>Detalhes</a>
+                </Space>
+              )}
+            />
+          </ColumnGroup>
+        </Table>
       </div>
     </section>
   );
