47cec465934fca0fb56adfdbdefd19899fc7345f
Joel Alves Pereira <atejap05@yahoo.com.br>
2022-12-13 13:54:39 -0400

    add isloggedin checker to header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-// import "antd/dist/antd.min.css";
 
 // Import de Components
 import Home from "./pages/Home";
@@ -13,17 +12,27 @@ import { ProtectedRoute } from "./components/ProtectedRoute";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/permuta" element={<ProtectedRoute Component={PermutaLayout} />}>
-        <Route path="/permuta/incluir" element={<ProtectedRoute Component={Incluir} />} />
-        <Route path="/permuta/consultar" element={<ProtectedRoute Component={Consultar} />} />
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/permuta"
+        element={<ProtectedRoute Component={PermutaLayout} />}
+      >
+        //TODO: estas rotas nao serão usadas!
         <Route
-    path="/permuta/manifestacoes"
-    element={<ProtectedRoute Component={Manifestacoes} />}
-    />
+          path="/permuta/incluir"
+          element={<ProtectedRoute Component={Incluir} />}
+        />
+        <Route
+          path="/permuta/consultar"
+          element={<ProtectedRoute Component={Consultar} />}
+        />
+        <Route
+          path="/permuta/manifestacoes"
+          element={<ProtectedRoute Component={Manifestacoes} />}
+        />
       </Route>
 
-      <Route path="*" element={<ErrorPage/>}/>
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 }
