45f184a25a1b0b64cc2e35533da1ff6e25d14bec
Joel Alves Pereira <atejap05@yahoo.com.br>
2022-12-13 11:26:14 -0400

    add comment

--- src/components/Layout/ChoosePic.jsx
+++ src/components/Layout/ChoosePic.jsx
@@ -5,6 +5,8 @@ const { Dragger } = Upload;
 const props = {
   name: "file",
   multiple: true,
+
+  // TODO: API endpoit here!!
   action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
   onChange(info) {
     const { status } = info.file;
